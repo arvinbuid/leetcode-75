1cca1a4a2196d46607d194aefa05576b2c40720a
Arvin Buid <arvinbuid01@gmail.com>
2025-10-15 22:53:04 +0800

    notes: add reverse-linked-list

---
+++ linked-list/reverse-linked-list.js
@@ -0,0 +1,19 @@
+// 206. Reverse Linked List
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+const reverseList = function (head) {
+  let node = null;
+
+  // [1, 2, 3, 4, 5]
+  while (head) {
+    const temp = head.next; // 2
+    head.next = node; // null 2
+    node = head;
+    head = temp;
+  }
+  return node;
+};
+
+// Test Case
+console.log(reverseList([1, 2, 3, 4, 5]));
