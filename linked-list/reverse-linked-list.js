5a579204bc29fd6a92d1eee97f2324d7d7fdd2b5
Arvin Buid <arvinbuid01@gmail.com>
2025-10-16 16:14:11 +0800

    sol: add recursive reverse linked list

--- linked-list/reverse-linked-list.js
+++ linked-list/reverse-linked-list.js
@@ -2,7 +2,9 @@
 
 // Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-const reverseList = function (head) {
+// In Leetcode, the function name should only be named 'reverseList' to pass the test cases.
+
+const reverseListIterative = function (head) {
   let node = null;
   // [1, 2, 3, 4, 5]
   while (head) {
@@ -12,4 +14,16 @@ const reverseList = function (head) {
     head = temp;
   }
   return node;
+};
+
+// Recursive Solution
+const reverseListRecursive = function (head) {
+  if (!head) return null; // base case
+  let newHead = head;
+  if (head.next) {
+    newHead = reverseListRecursive(head.next); // call recursively
+    head.next.next = head; // reverse the link between next node and head
+  }
+  head.next = null;
+  return newHead;
 };
